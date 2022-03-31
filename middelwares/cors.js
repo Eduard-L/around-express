7deb998ec2bc289d4b6571fff44e53b0d174fdfd
Eduard Loktev <lwqtbdwrd@gmail.com>
2022-03-31 12:50:25 +0300

    add cors preflight request

--- middelwares/cors.js
+++ middelwares/cors.js
@@ -7,14 +7,23 @@ const allowedCors = [
   'http://api.spartan-warrior.students.nomoreparties.sbs',
   'localhost:3000'
 ];
+const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
 function handleCors(req, res, next) {
   const { origin } = req.headers;
+  const { method } = req
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin)
   }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
 
   next();
 }
+
 module.exports = { handleCors }
